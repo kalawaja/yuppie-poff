9d6f50b8d3dd39fd3cd739734c0594986e3462c0
Ramazan İLTER <remzilter@gmail.com>
2023-04-20 02:29:33 +0300

    chart was displayed on the home in sync with db

--- index.php
+++ index.php
@@ -1,8 +1,90 @@
-<?php require_once "header.php"; ?>
+<?php require_once "header.php";
+
+require_once 'sorgu.php';
+
+?>
 <div class="row mt-2 mb-2">
     <div class="col-sm-12">
         <div class="d-flex">
-            <canvas id="chart" height="400px" width="100%"></canvas>
+            <canvas id="chart" height="20px" width="100%"></canvas>
+            <script>
+                var chart = document.getElementById("chart").getContext('2d');
+                var myChart = new Chart(chart, {
+                    type: 'line',
+                    data: {
+                        labels: ['Ocak', 'Şubat', 'Mart', 'Nisan', 'Mayıs', 'Haziran', 'Temmuz', 'Ağustos', 'Eylül', 'Ekim', 'Kasım', 'Aralık'],
+                        datasets: [{
+                                label: "Mutlu",
+                                data: [
+                                    <?php echo $mutluocakyazisaycek['say'] ?>,
+                                    <?php echo $mutlusubatyazisaycek['say'] ?>,
+                                    <?php echo $mutlumartyazisaycek['say'] ?>,
+                                    <?php echo $mutlunisanyazisaycek['say'] ?>,
+                                    <?php echo $mutlumayisyazisaycek['say'] ?>,
+                                    <?php echo $mutluhaziranyazisaycek['say'] ?>,
+                                    <?php echo $mutlutemmuzyazisaycek['say'] ?>,
+                                    <?php echo $mutluagustosyazisaycek['say'] ?>,
+                                    <?php echo $mutlueylulyazisaycek['say'] ?>,
+                                    <?php echo $mutluekimyazisaycek['say'] ?>,
+                                    <?php echo $mutlukasimyazisaycek['say'] ?>,
+                                    <?php echo $mutluaralikyazisaycek['say'] ?>
+
+                                ],
+                                borderColor: [
+                                    '#123edf',
+
+                                ]
+                            },
+                            {
+                                label: "Notr",
+                                data: [
+                                    <?php echo $notrocakyazisaycek['say'] ?>,
+                                    <?php echo $notrsubatyazisaycek['say'] ?>,
+                                    <?php echo $notrmartyazisaycek['say'] ?>,
+                                    <?php echo $notrnisanyazisaycek['say'] ?>,
+                                    <?php echo $notrmayisyazisaycek['say'] ?>,
+                                    <?php echo $notrhaziranyazisaycek['say'] ?>,
+                                    <?php echo $notrtemmuzyazisaycek['say'] ?>,
+                                    <?php echo $notragustosyazisaycek['say'] ?>,
+                                    <?php echo $notreylulyazisaycek['say'] ?>,
+                                    <?php echo $notrekimyazisaycek['say'] ?>,
+                                    <?php echo $notrkasimyazisaycek['say'] ?>,
+                                    <?php echo $notraralikyazisaycek['say'] ?>
+                                ],
+                                borderColor: [
+                                    '#ff7f00',
+
+                                ]
+                            },
+                            {
+                                label: "Üzgün",
+                                data: [
+                                    <?php echo $uzgunocakyazisaycek['say'] ?>,
+                                    <?php echo $uzgunsubatyazisaycek['say'] ?>,
+                                    <?php echo $uzgunmartyazisaycek['say'] ?>,
+                                    <?php echo $uzgunnisanyazisaycek['say'] ?>,
+                                    <?php echo $uzgunmayisyazisaycek['say'] ?>,
+                                    <?php echo $uzgunhaziranyazisaycek['say'] ?>,
+                                    <?php echo $uzguntemmuzyazisaycek['say'] ?>,
+                                    <?php echo $uzgunagustosyazisaycek['say'] ?>,
+                                    <?php echo $uzguneylulyazisaycek['say'] ?>,
+                                    <?php echo $uzgunekimyazisaycek['say'] ?>,
+                                    <?php echo $uzgunkasimyazisaycek['say'] ?>,
+                                    <?php echo $uzgunaralikyazisaycek['say'] ?>,
+                                ],
+                                borderColor: [
+                                    '#ff0000',
+
+                                ]
+                            }
+
+                        ]
+                    }
+                });
+            </script>
+
+
+
         </div>
         <h2 class="mt-2 mb-2">What Do You Think?</h2>
         <form action="netting/islem.php" method="post">
@@ -34,7 +116,7 @@
         <h2 class="mt-2 mb-2">Latest Updates</h2>
         <div id="sonuc" class="list-group mt-2">
             <?php
-            $yazisor = $db->prepare("SELECT yazi.*,kullanici.* FROM yazi INNER JOIN kullanici ON yazi.kullanici_id=kullanici.kullanici_id where yazi_durum=:durum order by yazi_zaman DESC");
+            $yazisor = $db->prepare("SELECT yazi.*,kullanici.* FROM yazi INNER JOIN kullanici ON yazi.kullanici_id=kullanici.kullanici_id where yazi_durum=:durum order by yazi_zaman DESC limit 10");
 
             $yazisor->execute([
                 'durum' => 1
@@ -43,17 +125,27 @@
                 $zaman = $yazicek['yazi_zaman'];
                 $sonuc = explode(" ", $zaman);
             ?>
-
-                <div class="list-group-item list-group-item-action"><span class="badge pill text-bg-primary"><?php echo $sonuc[1] ?></span>
-                    <span class="badge pill text-bg-dark"><?php echo $yazicek['kullanici_ad'] ?></span> <span class="badge pill text-bg-light"><?php echo $yazicek['yazi_detay'] ?></span>
-                </div>
-            <?php } ?>
+                <?php if ($yazicek['yazi_secenek'] == 0) { ?>
+                    <div class="list-group-item list-group-item-action bg-success"><span class="badge pill text-bg-primary"><?php echo $sonuc[1] ?></span>
+                        <span class="badge pill text-bg-dark"><?php echo $yazicek['kullanici_ad'] ?></span> <span class="badge pill text-bg-light"><?php echo $yazicek['yazi_detay'] ?></span>
+                    </div>
+                <?php } elseif ($yazicek['yazi_secenek'] == 1) { ?>
+                    <div class="list-group-item list-group-item-action bg-warning"><span class="badge pill text-bg-primary"><?php echo $sonuc[1] ?></span>
+                        <span class="badge pill text-bg-dark"><?php echo $yazicek['kullanici_ad'] ?></span> <span class="badge pill text-bg-light"><?php echo $yazicek['yazi_detay'] ?></span>
+                    </div>
+                <?php } elseif ($yazicek['yazi_secenek'] == 2) { ?>
+                    <div class="list-group-item list-group-item-action bg-danger"><span class="badge pill text-bg-primary"><?php echo $sonuc[1] ?></span>
+                        <span class="badge pill text-bg-dark"><?php echo $yazicek['kullanici_ad'] ?></span> <span class="badge pill text-bg-light"><?php echo $yazicek['yazi_detay'] ?></span>
+                    </div>
+            <?php  }
+            } ?>
         </div>
     </div>
 </div>
 <?php require_once 'footer.php' ?>
 </div>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
-<script src="script.js"></script>
 </body>
+
 </html>
